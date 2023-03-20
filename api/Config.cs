6e2cccb9815000cd6da7cb7ef4bf3852af4e90cb
Kincaid Keating <kincaidkeating@MacBook-Pro-3.local>
2023-03-20 12:33:18 -0500

    com2

---
+++ api/Config.cs
@@ -0,0 +1,12 @@
+namespace api
+{
+    public class Config
+    {
+        //will eventually hold API urls, database passwords, IP addresses, etc.
+        public string cs {get;set;}
+        public Config()
+        {
+            cs= @"URI=/Users/kincaidkeating/Desktop/SOURCE/spring23/fullstack/api/Config.cs/test.db";
+        }
+    }
+}
